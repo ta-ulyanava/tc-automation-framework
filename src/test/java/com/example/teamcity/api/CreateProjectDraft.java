47d27d3bf9523ae4bc9cc8d6a4cdeecd0ba218b7
ta-ulyanava <ta.ulyanava@gmail.com>
2025-03-28 12:56:17 +0100

    Add jackson dependency

--- src/test/java/com/example/teamcity/api/CreateProjectDraft.java
+++ src/test/java/com/example/teamcity/api/CreateProjectDraft.java
@@ -1,36 +1,40 @@
 package com.example.teamcity.api;
 
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import io.restassured.http.ContentType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.util.Map;
 
 
 public class CreateProjectDraft {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         RestAssured.baseURI = "http://192.168.1.34:8111";
         RestAssured.authentication = RestAssured.basic("admin", "admin");
 
-        String jsonBody = "{\n" +
-                "    \"parentProject\": {\n" +
-                "        \"locator\": \"_Root\"\n" +
-                "    },\n" +
-                "    \"name\": \"project100\",\n" +
-                "    \"id\": \"project100\",\n" +
-                "    \"copyAllAssociatedSettings\": true\n" +
-                "}";
+        Map<String, Object> payload = Map.of(
+                "parentProject", Map.of("locator", "_Root"),
+                "name", "project100",
+                "id", "project100",
+                "copyAllAssociatedSettings", true
+        );
 
-        Response response = RestAssured
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonBody = mapper.writeValueAsString(payload);
+
+        RestAssured
                 .given()
-                .log().all()
-                .contentType(ContentType.JSON)
-                .accept(ContentType.JSON)
-                .body(jsonBody)
+                    .log().all()
+                    .contentType(ContentType.JSON)
+                    .accept(ContentType.JSON)
+                    .body(jsonBody)
                 .when()
-                .post("/app/rest/projects")
+                    .post("/app/rest/projects")
                 .then()
-                .log().all()
-                .extract().response();
+                    .log().all()
+                    .extract().response();
 
     }
 }
